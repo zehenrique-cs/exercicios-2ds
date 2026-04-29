37cb2e3ec16b355e7bf999e6bedd6157112804a7
josehenrique <jose.duarte95@aluno.ce.gov.br>
2026-04-29 17:51:57 +0000

    pto

---
+++ 2-bimestre/programacao-web/projetos/projeto5-primeiro-site-com-react/react/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function App(){
+    return(
+        <div>
+            <h1>Meu Primeiro Site</h1>
+            <a href="home">Home</a>
+             <a href ="sobre">Sobre</a>
+             <a href="contato">Contato</a>
+        </div>
+    )
+}
